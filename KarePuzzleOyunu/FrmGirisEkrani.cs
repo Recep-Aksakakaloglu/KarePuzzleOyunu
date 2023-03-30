0a1d9a4221442d8a2ee5ccd723438eeb3401f455
Recep Aksakaloğlu <74347231+Recep-Aksakakaloglu@users.noreply.github.com>
2023-03-30 22:49:57 +0300

    Update_Project_By_Recep

---
+++ KarePuzzleOyunu/FrmGirisEkrani.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KarePuzzleOyunu
+{
+    public partial class FrmGirisEkrani : Form
+    {
+        public FrmGirisEkrani()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtAdSoyad.Text == "")
+            {
+                MessageBox.Show("Adınızı Soyadınızı Giriniz", "Hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                FrmOyunEkrani frmOyunEkrani = new FrmOyunEkrani(txtAdSoyad.Text);
+                frmOyunEkrani.Show();
+                this.Hide();
+            }
+        }
+    }
+}
